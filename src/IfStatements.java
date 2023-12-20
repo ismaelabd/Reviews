7349ba0c1a65ade237f1aff6e0c6c09b6198b32b
ismaelabd <ismael.abed-ali1@stu.ocsb.ca>
2023-12-20 11:11:10 -0500

    Update IfStatements.java

--- src/IfStatements.java
+++ src/IfStatements.java
@@ -6,66 +6,161 @@ public class IfStatements {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner (System.in);
 int yes = 0;
-		
+int word=0;
+int picture = 0;
+int nature = 0;
+int math = 0;
+int people = 0;
+int music = 0;
+int self = 0;
+int body = 0;
 	System.out.println("Intelligences Survey");
 	System.out.println("Answer the following questions with 1 for Yes and 2 for No");
 	
 	System.out.println ("1. I like to write Surveys");
-	int response = in.nextInt();
+	int response1 = in.nextInt();
+	if (response1==1) {
+		word++;
+	}
 	System.out.println ("2. I like to take pictures");
+	int response2 = in.nextInt();
+	if (response2==1) {
+		picture++;
+	}
 	System.out.println ("3. I like to plant seeds and grow plants");
-	System.out.println ("4. I like to count");
-	System.out.println ("5. I like to check the weather");
-	System.out.println ("6. I like to talk to others about ideas");
-	System.out.println ("7. I like to read in my free time");
-	System.out.println ("8. I like to dance");
-	System.out.println ("9. I like to read maps");
-	System.out.println ("10. I like to figure out patterns");
-	System.out.println ("11. I like to take turns");
-	System.out.println ("12. I like to solve puzzles");
-	System.out.println ("13. I like to sing");
-	System.out.println ("14. I like to ake things with my hands");
-	System.out.println ("15. I like to talk to others");
-	System.out.println ("16. I like poetry and rhyming");
-	System.out.println ("17. I like to be by myself sometimes");
-	System.out.println ("18. I like to touch different materials");
-	System.out.println ("19. I like to move around a lot");
-	System.out.println ("20. I like to play alone");
-	System.out.println ("21. I like to play word games");
-	System.out.println ("22. i like taking care of animals");
-	System.out.println ("23. I like to draw");
-	System.out.println ("24. I am wise");
-		
-		if (response ==1) {
-			yes++;
-		}
-	 while (response != 0);
-	System.out.println("Total number of yeses: " + yes);
+	int response3 = in.nextInt();
+	if (response3==1) {
+		nature++;
+	}
 	
-	if (yes == 13 ||yes == 16|| yes == 8) {
-		System.out.println("Your learning style is Music!");
+	System.out.println ("4. I like to count");
+	int response4 = in.nextInt();
+	if (response4==1) {
+		math++;
 	}
-	else if (yes == 14 ||yes == 18|| yes == 19) {
-		System.out.println("Your learning style is Body!");
+	
+	System.out.println ("5. I like to check the weather");
+	int response5 = in.nextInt();
+	if (response5==1) {
+		nature++;
 	}
-	else if (yes == 3 ||yes == 22|| yes == 5) {
-		System.out.println("Your learning style is Nature!");
+	
+	System.out.println ("6. I like to talk to others about ideas");
+	int response6 = in.nextInt();
+	if (response6==1) {
+		people++;
 	}
-	else if (yes == 1 ||yes == 7|| yes == 21) {
-		System.out.println("Your learning style is Word!");
+	
+	System.out.println ("7. I like to read in my free time");
+	int response7 = in.nextInt();
+	if (response7==1) {
+		word++;
 	}
-	else if (yes == 4 ||yes == 10|| yes == 12) {
-		System.out.println("Your learning style is Math!");
+	
+	System.out.println ("8. I like to dance");
+	int response8 = in.nextInt();
+	if (response8==1) {
+		music++;
 	}
-	else if (yes == 17 ||yes == 20|| yes == 24) {
-		System.out.println("Your learning style is Self!");
+	
+	System.out.println ("9. I like to read maps");
+	int response9 = in.nextInt();
+	if (response9==1) {
+		picture++;
 	}
-	else if (yes == 11 ||yes == 6|| yes == 15) {
-		System.out.println("Your learning style is People!");
+	
+	System.out.println ("10. I like to figure out patterns");
+	int response10 = in.nextInt();
+	if (response10==1) {
+		math++;
 	}
-	else if (yes == 2 ||yes == 9|| yes == 23) {
-		System.out.println("Your learning style is Picture!");
-		}
+	
+	System.out.println ("11. I like to take turns");
+	int response11 = in.nextInt();
+	if (response11==1) {
+		people++;
+	}
+	
+	System.out.println ("12. I like to solve puzzles");
+	int response12 = in.nextInt();
+		if (response12==1) {
+			math++;
+	}
+	
+	System.out.println ("13. I like to sing");
+	int response13 = in.nextInt();
+	if (response13==1) {
+		music++;
+	}
+	
+	System.out.println ("14. I like to ake things with my hands");
+	int response14 = in.nextInt();
+	if (response14==1) {
+		body++;
+	}
+	
+	System.out.println ("15. I like to talk to others");
+	int response15 = in.nextInt();
+	if (response15==1) {
+		people++;
+	}
+	
+	System.out.println ("16. I like poetry and rhyming");
+	int response16 = in.nextInt();
+	if (response16==1) {
+		music++;
+	}
+	
+	System.out.println ("17. I like to be by myself sometimes");
+	int response17 = in.nextInt();
+	if (response17==1) {
+		self++;
+	}
+	
+	System.out.println ("18. I like to touch different materials");
+	int response18 = in.nextInt();
+	if (response18==1) {
+		body++;
+	}
+	
+	System.out.println ("19. I like to move around a lot");
+	int response19 = in.nextInt();
+	if (response19==1) {
+		body++;
+	}
+	
+	System.out.println ("20. I like to play alone");
+	int response20 = in.nextInt();
+	if (response20==1) {
+		self++;
+	}
+	
+	System.out.println ("21. I like to play word games");
+	int response21 = in.nextInt();
+	if (response21==1) {
+		word++;
+	}
+	
+	System.out.println ("22. i like taking care of animals");
+	int response22 = in.nextInt();
+	if (response22==1) {
+		nature++;
+	}
+	
+	System.out.println ("23. I like to draw");
+	int response23 = in.nextInt();
+	if (response23==1) {
+		picture++;
+	}
+	
+	System.out.println ("24. I am wise");
+	int response24 = in.nextInt();
+	if (response24==1) {
+		self++;
+	}
+		
+	
+
 	in.close();
 	}
 }
